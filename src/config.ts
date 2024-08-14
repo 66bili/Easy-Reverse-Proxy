8f4265ac7498429d57a5da8db918c53821c600e6
66bili <63287602+66bili@users.noreply.github.com>
2024-08-14 17:29:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export interface Proxy {
 
 export let proxys: Proxy[] = [
     {
-        domain: "ggproxy-one.vercel.app/",
+        domain: "ggproxy-one.vercel.app",
         url: "https://www.google.com/"
     }
 ];
