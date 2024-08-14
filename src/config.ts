fac97b74bb09a125344353441118aeb6f27724f6
66bili <63287602+66bili@users.noreply.github.com>
2024-08-14 17:51:34 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,4 +7,6 @@ export let proxys:
     Proxy[] = [ {domain: "ggproxy-one.vercel.app",url: "https://www.google.com/" }];
 export let proxys: 
     Proxy[] = [ {domain: "gg.ananyo.fun",url: "https://www.google.com/" }];
+export let proxys: 
+    Proxy[] = [ {domain: "alhs.ananyo.fun",url: "https://www.alhs.xyz/" }];
    
