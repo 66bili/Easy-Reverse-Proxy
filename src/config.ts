d2135e76dd451b5d97287bf16a9d43313e19663c
66bili <63287602+66bili@users.noreply.github.com>
2024-08-14 17:53:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,4 +9,6 @@ export let proxys:
     Proxy[] = [ {domain: "gg.ananyo.fun",url: "https://www.google.com/" }];
 export let proxys: 
     Proxy[] = [ {domain: "alhs.ananyo.fun",url: "https://www.alhs.xyz/" }];
+export let proxys: 
+    Proxy[] = [ {domain: "h.ananyo.fun",url: "https://www.hanime1.me/" }];
    
