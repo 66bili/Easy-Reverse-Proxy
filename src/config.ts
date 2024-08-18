5f95e0f7307a0ebdd61d62c2643494edc393ae8c
66bili <63287602+66bili@users.noreply.github.com>
2024-08-18 10:37:52 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export let proxys:
 export let proxys: 
     Proxy[] = [ {domain: "gg.ananyo.fun",url: "https://www.google.com/" }];
 export let proxys: 
-    Proxy[] = [ {domain: "so.ananyo.fun",url: "https://bing.com/" }];
+    Proxy[] = [ {domain: "so.ananyo.fun",url: "https://www.bing.com/" }];
 export let proxys: 
     Proxy[] = [ {domain: "nili.ananyo.fun",url: "https://nilinl.pro/" }];
 export let proxys: 
